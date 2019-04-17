5285b1655265560878567998ce1d7be5e9e77a25
ilyaagarkov <ilya.al.ag@gmail.com>
2019-04-18 00:50:37 +0500

    add cert

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require('path');
+const fs = require('fs')
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: [
+    './index.js',
+    'webpack-dev-server/client?http://0.0.0.0:80',
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    https: true,
+    https: {
+      key: fs.readFileSync('/Users/ilyaagarkov/projects/devvela/https_test/localhost.key'),
+      cert: fs.readFileSync('/Users/ilyaagarkov/projects/devvela/https_test/localhost.crt'),
+    },
+    allowedHosts: [
+      '.bomond.devvela.net'
+    ],
     hot: true,
     clientLogLevel: 'warning',
     compress: false,
     publicPath: '/',
-    port: 3002,
+    port: 443,
+    host: 'localhost',
     watchOptions: {
       aggregateTimeout: 100,
       ignored: /node_modules/,
